da823b68395bb36fecd3e4ecdccb16314af8d391
naveen <nsrinivasan1976@gmail.com>
2015-12-27 18:30:12 -0500

    Fixes for second problem

--- 1.fsx
+++ 1.fsx
@@ -1,5 +1,6 @@
 open System.IO
 let puzzle = File.ReadAllText( Path.Combine(__SOURCE_DIRECTORY__ , "1.txt"))
+
 let runningTotal seq' = (Seq.head seq', Seq.skip 1 seq') ||> Seq.scan (+)
 let nums= puzzle |> Seq.map(fun f -> match f.ToString() with
                                                        | "(" -> 1
@@ -7,12 +8,13 @@ let nums= puzzle |> Seq.map(fun f -> match f.ToString() with
                                                        |_ -> 0)
 
                                                                                                                                                                        
-let ``first-1`` = (nums |> runningTotal |> Seq.indexed |> Seq.filter(fun f -> snd(f) = -1 ) |> Seq.map(fun f -> fst f)  |> Seq.head) + 1
+let ``first-1`` = (nums 
+                    |> runningTotal 
+                    |> Seq.indexed 
+                    |> Seq.filter(fun f -> snd(f) = -1 ) 
+                    |> Seq.map(fun f -> fst f)  
+                    |> Seq.head) + 1
                                                       
 printfn "Santa would be in %i floor"(nums |> Seq.reduce(+))
 
-printfn "First occurence -1 at position %i " ``first-1``
-
-
-
-
+printfn "First occurence -1 at position %i " ``first-1`` 
---
+++ 2.fsx
@@ -0,0 +1,23 @@
+open System
+open System.IO
+
+type Rect = {
+  Length : int;
+  Width  : int;
+  Height : int
+}
+
+let parse (s:string) =
+  let splitstring = s.Split([|'x'|])
+  {Length = int splitstring.[0]; Width = int splitstring.[1]; Height = int splitstring.[2]}
+
+let prismarea (r:Rect) =
+  let calculations = [(2 * r.Length * r.Width); (2 * r.Width * r.Height); (2 * r.Height * r.Length)] 
+  let lowest = (calculations |> List.min) / 2
+  lowest::calculations |> List.reduce(+) 
+
+let area = parse >> prismarea
+
+let totalarea =File.ReadAllLines (Path.Combine(__SOURCE_DIRECTORY__, "2.txt")) |> Seq.map(fun f-> area f) |> Seq.reduce(+)
+
+printfn "Total area required is %i" totalarea 
