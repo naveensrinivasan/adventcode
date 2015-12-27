ce8bd197b5f4701eb0d1af46045f51be6f57834f
naveen <nsrinivasan1976@gmail.com>
2015-12-26 22:29:05 -0500

    Part 2 of day 1 problem

--- 1.fsx
+++ 1.fsx
@@ -1,7 +1,18 @@
 open System.IO
-let puzzle = File.ReadAllText( Path.Combine(__SOURCE_DIRECTORY__ , "1.txt")) 
+let puzzle = File.ReadAllText( Path.Combine(__SOURCE_DIRECTORY__ , "1.1.txt"))
+let runningTotal seq' = (Seq.head seq', Seq.skip 1 seq') ||> Seq.scan (+)
 let nums= puzzle |> Seq.map(fun f -> match f.ToString() with
                                                        | "(" -> 1
                                                        | ")" -> -1
-                                                       |_ -> 0) |> Seq.reduce(+)
- printfn "%i " nums
+                                                       |_ -> 0)
+
+                                                                                                                                                                       
+let ``first-1`` = (nums |> runningTotal |> Seq.indexed |> Seq.filter(fun f -> snd(f) = -1 ) |> Seq.map(fun f -> fst f)  |> Seq.head) + 1
+                                                      
+printfn "Santa would be in %i floor"(nums |> Seq.reduce(+))
+
+printfn "First occurence -1 at position %i " ``first-1``
+
+
+
+
