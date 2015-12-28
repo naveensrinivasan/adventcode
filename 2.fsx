09aed75f4807890e7109ac027bbfef4cce0d9b49
naveen <nsrinivasan1976@gmail.com>
2015-12-27 20:47:05 -0500

    Solution for the second problem for day 2

--- 2.fsx
+++ 2.fsx
@@ -21,3 +21,16 @@ let area = parse >> prismarea
 let totalarea =File.ReadAllLines (Path.Combine(__SOURCE_DIRECTORY__, "2.txt")) |> Seq.map(fun f-> area f) |> Seq.reduce(+)
 
 printfn "Total area required is %i" totalarea 
+
+
+let ribbonLength (r:Rect) =
+        let list = [|r.Height;r.Width;r.Length|]
+        let bow = list |> Array.reduce (*)
+        let [|l;w|] =  Array.sort(list).[0..1]
+        let wrap = (l + l) + (w+w) 
+        bow+wrap
+
+let ribbonsize = parse >> ribbonLength
+let totalRibbonLength =File.ReadAllLines (Path.Combine(__SOURCE_DIRECTORY__, "2.txt")) |> Seq.map(fun f-> ribbonsize f) |> Seq.reduce(+)
+
+printfn "Total Ribbon length required is %i " totalRibbonLength
