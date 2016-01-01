cf6135c40e3e9b260c7d4fe6b6e427938e76c0e4
naveen <nsrinivasan1976@gmail.com>
2016-01-01 18:04:18 -0500

    Fixes day 6 second problem

--- 6.fsx
+++ 6.fsx
@@ -1,47 +1,28 @@
 open System.IO
 open System.Text.RegularExpressions
 
-(**
-
---- Day 6: Probably a Fire Hazard ---
-
-Because your neighbors keep defeating you in the holiday house decorating contest year after year, you've decided to deploy one million lights in a 1000x1000 grid.
-
-Furthermore, because you've been especially nice this year, Santa has mailed you instructions on how to display the ideal lighting configuration.
-
-Lights in your grid are numbered from 0 to 999 in each direction; the lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. Each coordinate pair represents opposite corners of a rectangle, inclusive; a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. The lights all start turned off.
-
-To defeat your neighbors this year, all you have to do is set up your lights by doing the instructions Santa sent you in order.
-
-For example:
-
-turn on 0,0 through 999,999 would turn on (or leave on) every light.
-toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
-turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
-After following the instructions, how many lights are lit?
-**)
-
 let filereadlines f = File.ReadAllLines(Path.Combine(__SOURCE_DIRECTORY__, f ))
 
-type Switch = On | Off 
-
+type Switch = On | Off | Brightness of int
+        
 type Action = On | Off | Toggle
+
+type MessageLanguage = Nordic | English
 
 type Instruction = {
   Operation : Action ;
   StartRow  : int;
   StartCol : int;
   EndRow : int;
-  EndCol : int
+  EndCol : int;
+  Language  : MessageLanguage
 }
-
-let lights =Array2D.create 1000 1000 Switch.Off
 
 let toggle = function
             | Switch.On -> Switch.Off 
-            |_  -> Switch.On 
+            |_  ->  Switch.On 
 
-let getInstruction (line:string)= 
+let getInstruction (line:string) (lang:MessageLanguage)= 
                   let matches = Regex.Matches(line, "[\w\d_]+")
                                         |> Seq.cast<Match>
                                         |> Seq.filter (fun f -> f.Success) |> Seq.map(fun f-> f.Value) 
@@ -49,25 +30,55 @@ let getInstruction (line:string)=
                   let action = if matches |> Seq.nth 0 = "toggle" then Toggle
                                   elif matches |> Seq.nth 1 = "on"   then  On
                                   else Off 
-                  let elementat n= int(matches |> Seq.nth n)
+
+                  let elementat n= matches |> Seq.nth n |> int
 
                   match action with
                   |Toggle -> {Operation = action; StartRow = elementat 1; StartCol = elementat 2; 
-                                EndRow = elementat 4 ; EndCol = elementat 5}
+                                EndRow = elementat 4 ; EndCol = elementat 5; Language = lang}
                   |_ -> {Operation = action; StartRow = elementat 2; StartCol = elementat 3;
-                          EndRow = elementat 5 ; EndCol = elementat 6} 
+                          EndRow = elementat 5 ; EndCol = elementat 6; Language= lang} 
+
+let translateCode (state:Switch) (lang :MessageLanguage) (action:Action) =
+    match lang,action,state with
+    |English, Toggle,_ ->  toggle state 
+    |English, On,_ ->   Switch.On 
+    |Nordic, Toggle,Brightness(x)  ->  Brightness (x+2) 
+    |Nordic, On,Brightness(x) ->  Brightness (x+1) 
+    |Nordic, Off ,Brightness(x) when x = 0 ->  Brightness (0) 
+    |Nordic, Off ,Brightness(x)  ->  Brightness (x-1) 
+    |_,_,_ ->  Switch.Off 
+
+let lights =Array2D.create 1000 1000 Switch.Off
 
 "6.txt"
 |> filereadlines
-|> Seq.map getInstruction
+|> Seq.map (fun f -> getInstruction f English)
 |> Seq.iter(fun ins -> 
         for i in  ins.StartRow .. ins.EndRow do
             for j in ins.StartCol .. ins.EndCol  do
-                match ins.Operation with
-                |Toggle ->  lights.[i, j] <- (toggle (lights.[i,j]))
-                |On ->  lights.[i, j] <- Switch.On
-                |_ -> lights.[i,j] <- Switch.Off
-        ) |> ignore
+                lights.[i, j] <- translateCode lights.[i, j] ins.Language ins.Operation)
 
-printfn "Lights that were turned on are %i"  
-    (lights |> Seq.cast<Switch> |> Seq.filter(fun f -> f = Switch.On) |> Seq.length) 
+lights 
+|> Seq.cast<Switch> 
+|> Seq.filter(fun f -> f = Switch.On)
+|> Seq.length
+|> printfn "The number of lights that are turned on are %i"
+
+
+let nordiclights =Array2D.create 1000 1000 (Brightness 0)
+"6.txt"
+|> filereadlines
+|> Seq.map (fun f -> getInstruction f Nordic)
+|> Seq.iter(fun ins -> 
+        for i in  ins.StartRow .. ins.EndRow do
+            for j in ins.StartCol .. ins.EndCol  do
+                nordiclights.[i, j] <- translateCode nordiclights.[i, j] ins.Language ins.Operation)
+
+nordiclights 
+|> Seq.cast<Switch> 
+|> Seq.map(fun f -> match f with
+                       |Brightness(x) -> x
+                       |_ -> 0)
+|> Seq.sum 
+|> printfn "The Santa's real nordic decoded message and the total brightness is %i"
